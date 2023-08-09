f351e3b95fc9351c244295ba6089756e1597da2d
SageScroll18144 <ffss@cin.ufpe.br>
2023-08-09 10:43:57 -0300

    xor e or

--- main/alu.sv
+++ main/alu.sv
@@ -17,10 +17,16 @@ module alu#(
             case(Operation)
             4'b0000:        // AND
                     ALUResult = SrcA & SrcB;
+            4'b0001:    //OR
+                    ALUResult = SrcA ^ SrcB; 
             4'b0010:        // ADD
                     ALUResult = SrcA + SrcB;
+            4'b0011:           //OR
+                    ALUResult = SrcA | SrcB;  
+            4'b0100:    //SUB
+                    ALUResult = SrcA - SrcB; 
             4'b1000:        // Equal
-                    ALUResult = (SrcA == SrcB) ? 1 : 0;
+                    ALUResult = (SrcA == SrcB) ? 1 : 0;   
             default:
                     ALUResult = 0;
             endcase
