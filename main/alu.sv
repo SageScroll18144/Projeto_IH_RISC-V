fce75e1189d604dc0b23f386c4d9b3bc627f47a7
Mend25 <113634496+Mend25@users.noreply.github.com>
2023-08-10 10:24:01 -0300

    Adicionando SLT

--- main/alu.sv
+++ main/alu.sv
@@ -27,6 +27,8 @@ module alu#(
                     ALUResult = SrcA - SrcB;
             4'b1000:        // Equal
                     ALUResult = (SrcA == SrcB) ? 1 : 0;   
+            4'b1001: //SLT
+			ALUResult = SrcA << SrcB;
             default:
                     ALUResult = 0;
             endcase
