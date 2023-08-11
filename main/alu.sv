359a00e2bd22cc79277bb80c5acb86d63e3ace67
SageScroll18144 <ffss@cin.ufpe.br>
2023-08-11 00:20:52 -0300

    adiciocando opcode no reg de escrita

--- main/alu.sv
+++ main/alu.sv
@@ -19,12 +19,12 @@ module alu#(
                     ALUResult = SrcA & SrcB;
             4'b0001:    //XOR
                     ALUResult = SrcA ^ SrcB; 
-            4'b0010:        // ADD or ADDI
-                    ALUResult = SrcA + SrcB;
+            4'b0010:        // SUB
+                    ALUResult = SrcA - SrcB;
             4'b0011:           //OR
                     ALUResult = SrcA | SrcB;  
-            4'b0100:    //SUB
-                    ALUResult = SrcA - SrcB;
+            4'b0100:    // ADD or ADDI
+                    ALUResult = SrcA + SrcB;
             4'b1000:        // Equal
                     ALUResult = (SrcA == SrcB) ? 1 : 0;   
             4'b1001: //SLT
