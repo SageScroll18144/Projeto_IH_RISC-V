967410f583a117d6359990463da2228324004b4e
GuilhermeMontt <113855003+GuilhermeMontt@users.noreply.github.com>
2023-08-20 01:09:32 -0300

    Update ALUController.sv
    transformamos em slli

--- main/ALUController.sv
+++ main/ALUController.sv
@@ -15,7 +15,7 @@ module ALUController (
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) || // R\I->>>
       ((ALUOp == 2'b10) && (Funct3 == 3'b100) && (Funct7 == 7'b0000000)) || //XOR
       ((ALUOp == 2'b10) && (Funct3 == 3'b110) && (Funct7 == 7'b0000000)) || //OR
-      ((ALUOp == 2'b10) && (Funct3 == 3'b010)) //SLT/STLI
+      ((ALUOp == 2'b10) && (Funct3 == 3'b001) && (Funct7 == 7'b0000000)) //SLLI
       ;
 
   assign Operation[1] = (ALUOp == 2'b00) ||  // LW\SW
@@ -33,7 +33,7 @@ module ALUController (
 
   assign Operation[3] = (ALUOp == 2'b01) ||  // BEQ
       ((ALUOp == 2'b10) && (Funct3 == 3'b010)) || //R\I-<
-      ((ALUOp == 2'b10) && (Funct3 == 3'b010))  || // SLT/STLI
+      ((ALUOp == 2'b10) && (Funct3 == 3'b001) && (Funct7 == 7'b0000000))  || // SLLI
       ((ALUOp == 2'b11)); //LUI
 
 endmodule
