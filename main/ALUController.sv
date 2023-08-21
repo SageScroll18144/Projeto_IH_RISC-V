572ef4d5833c5073570c21f91ce3d7e6c82c6d10
Vitor Manoel <114445111+vitork15@users.noreply.github.com>
2023-08-21 01:09:54 -0300

    implementando BLT, BNE e BGE

--- main/ALUController.sv
+++ main/ALUController.sv
@@ -15,7 +15,10 @@ module ALUController (
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) || // R\I->>>
       ((ALUOp == 2'b10) && (Funct3 == 3'b100) && (Funct7 == 7'b0000000)) || //XOR
       ((ALUOp == 2'b10) && (Funct3 == 3'b110) && (Funct7 == 7'b0000000)) || //OR
-      ((ALUOp == 2'b10) && (Funct3 == 3'b001) && (Funct7 == 7'b0000000)) //SLLI
+      ((ALUOp == 2'b10) && (Funct3 == 3'b001) && (Funct7 == 7'b0000000)) || //SLLI
+      ((ALUOp == 2'b01) && (Funct3 == 3'b100)) || // BLT 
+      ((ALUOp == 2'b01) && (Funct3 == 3'b101)) // BGE
+      
       ;
 
   assign Operation[1] = (ALUOp == 2'b00) ||  // LW\SW
@@ -23,7 +26,9 @@ module ALUController (
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||// R\I->>>
       ((ALUOp == 2'b10) && (Funct3 == 3'b110) && (Funct7 == 7'b0000000)) ||  //OR
       ((ALUOp == 2'b10) && (Funct3 == 3'b010) && (Funct7 == 7'b0000000)) ||  //SLT ou SLTI
-      ((ALUOp == 2'b11)); //LUI
+      ((ALUOp == 2'b11)) ||  //LUI
+      ((ALUOp == 2'b01) && (Funct3 == 3'b001)) // BNE
+      ;
 
   assign Operation[2] =  ((ALUOp==2'b10) && (Funct3==3'b101) && (Funct7==7'b0000000)) || // R\I->>
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||  // R\I->>>
@@ -32,6 +37,9 @@ module ALUController (
       ((ALUOp == 2'b10) && (Funct3 == 3'b010) && (Funct7 == 7'b0000000)) ||  //SLT ou SLTI
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000)) || //SRLI
       ((ALUOp == 2'b10) && (Funct3 == 3'b000) && (Funct7 != 7'b0100000)) //add ou addi
+      ((ALUOp == 2'b01) && (Funct3 == 3'b100)) || // BLT 
+      ((ALUOp == 2'b01) && (Funct3 == 3'b101)) || // BGE
+      ((ALUOp == 2'b01) && (Funct3 == 3'b001)) // BNE
       ;  
 
   assign Operation[3] = (ALUOp == 2'b01) ||  // BEQ
@@ -39,7 +47,8 @@ module ALUController (
       ((ALUOp == 2'b10) && (Funct3 == 3'b001) && (Funct7 == 7'b0000000))  || // SLLI
       ((ALUOp == 2'b10) && (Funct3 == 3'b010) && (Funct7 == 7'b0000000)) ||  //SLTI
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000)) || //SRLI
-      ((ALUOp == 2'b11)) //LUI
+      ((ALUOp == 2'b11)) || //LUI
+      ((ALUOp == 2'b01) && (Funct3 == 3'b100)) // BLT 
     //SRAI
       ;
        
