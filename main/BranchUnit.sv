af8cae7ea1f917c5dd28bcea132e368ca14eac02
Mend25 <113634496+Mend25@users.noreply.github.com>
2023-08-23 15:22:16 -0300

    Incluindo JalrSel

--- main/BranchUnit.sv
+++ main/BranchUnit.sv
@@ -7,6 +7,7 @@ module BranchUnit #(
     input logic [31:0] Imm,
     input logic Branch,
     input logic JSel,
+    input logic JalrSel,
     input logic [31:0] AluResult,
     output logic [31:0] PC_Imm,
     output logic [31:0] PC_Four,
@@ -19,9 +20,9 @@ module BranchUnit #(
 
   assign PC_Full = {23'b0, Cur_PC};
 
-  assign PC_Imm = PC_Full + Imm;
+  assign PC_Imm = (JalrSel == 1) ? ALUResult : (PC_Full + Imm); //Se for Jalr, pega o resultado de Alu. Se for Jal ou Branch, (PC_Full + Imm)
   assign PC_Four = PC_Full + 32'b100;
-  assign Branch_Sel = Branch && AluResult[0];  // 0:Branch is taken; 1:Branch is not taken
+  assign Branch_Sel = (Branch && AluResult[0]) || (JSel);  // 0:Branch is taken; 1:Branch is not taken
 
   assign BrPC = (Branch_Sel) ? PC_Imm : 32'b0;  // Branch -> PC+Imm   // Otherwise, BrPC vsim:/tb_top/#INITIAL#35alue is not important
   assign PcSel = Branch_Sel;  // 1:branch is taken; 0:branch is not taken(choose pc+4)
