b2a8f3314c8b536569f0bd4b15eb6d0a130c7a09
SageScroll18144 <ffss@cin.ufpe.br>
2023-08-24 15:43:44 -0300

    Revert "Adicionando JAL e JALR"
    This reverts commit d697f8eca24369c0c67660050fc3c42e017460cc.

--- main/imm_Gen.sv
+++ main/imm_Gen.sv
@@ -25,19 +25,6 @@ module imm_Gen (
         inst_code[11:8],
         1'b0
       };
-
-     7'b1100111:  /*Jalr-type*/
-          Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
-
-     7'b1101111: /*J-type*/
-     Imm_out = {
-        inst_code[31] ? 11'h7FF : 11'b0,
-        inst_code[31],
-        inst_code[19:12],
-        inst_code[20],
-        inst_code[30:21],
-        1'b0
-      };
       
       7'b0110111: //U-TYPE LUI
         Imm_out = {inst_code[31:12] , 12'b0};
